9338c4d484b24155255c16d914a00d6ab753a4e5
rcleyton <silva.rcleyton@gmail.com>
2021-02-28 21:01:23 -0300

    Done

---
+++ README.md
@@ -0,0 +1,10 @@
+# Básico de OOP Ruby
+* Classes
+* Objetos
+* Métodos
+* Associação
+* Herança
+
+# Finalidade
+Abstrair conceitos de transações bancárias para criar classes e objetos. Entender o estado e comportamento de objetos. Assim como associções e herança.
+
---
+++ conta_corrent.rb
@@ -0,0 +1,20 @@
+require_relative 'conta'
+
+class ContaCorrente < Conta
+
+  attr_accessor :limite
+
+  def initialize(numero, titular, saldo, limite)
+    super(numero, titular, saldo)
+    @limite = limite
+  end
+
+  def sacar(valor)
+    if (saldo + limite) >= valor
+      self.saldo -= valor
+    else 
+      puts "Saldo insuficiente"
+    end
+  end
+
+end
